42763cb0ead6c33bb1ea1c326549e7fa46ddf28a
Kazuaki Ishiguro <kazuakiishiguro@protonmail.com>
2025-03-09 21:47:26 +0900

    delete

--- ellm-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from ellm.el  -*- no-byte-compile: t -*-
-(define-package "ellm" "0.2.0" "Interactive coding assistant with local LLMs" '((emacs "27.1")) :authors '(("Kazuaki Ishiguro")) :maintainer '("Kazuaki Ishiguro") :keywords '("convenience" "tools" "processes") :url "https://github.com/kazuakiishiguro/ellm.el")
